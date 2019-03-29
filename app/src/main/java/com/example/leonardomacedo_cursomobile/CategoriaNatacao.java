7e992ef7c0229bb854702f232b95c60db23d375d
Leo <lleoll@gmail.com>
2019-03-29 12:17:00 -0300

    +Categoria Natação

--- app/src/main/java/com/example/leonardomacedo_cursomobile/CategoriaNatacao.java
+++ app/src/main/java/com/example/leonardomacedo_cursomobile/CategoriaNatacao.java
@@ -2,12 +2,61 @@ package com.example.leonardomacedo_cursomobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CategoriaNatacao extends AppCompatActivity {
+
+    private EditText idadeTxt;
+    private TextView categoriaEscolhidaTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categoria_natacao);
+        idadeTxt = findViewById(R.id.editIdadeNatacao);
+        categoriaEscolhidaTxt = findViewById(R.id.textCategoria);
+    }
+
+    public void determinaCategoria(View v)
+    {
+        int idade = Integer.parseInt(idadeTxt.getText().toString());
+        String categoria;
+        if (idade < 5)
+            categoria = "Você ainda é um bebê, vá brincar.";
+        else
+        {
+            if (idade <= 7)
+            {
+                categoria = "Você está na categoria Infantil A.";
+            }
+            else
+            {
+                if (idade <= 10)
+                {
+                    categoria = "Você está na categoria Infantil B.";
+                }
+                else
+                {
+                    if (idade <= 13)
+                    {
+                        categoria = "Você está na categoria Juvenil A.";
+                    }
+                    else
+                    {
+                        if (idade <= 17)
+                        {
+                            categoria = "Você está na categoria Juvenil B.";
+                        }
+                        else
+                        {
+                            categoria = "Você está na categoria Sênior.";
+                        }
+                    }
+                }
+            }
+        }
+        categoriaEscolhidaTxt.setText(categoria);
     }
 }
